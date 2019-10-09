0e68c645c015d73335a56f639f808c9635916d43
Keshav <kshavgupta47@gmail.com>
2019-10-09 17:28:50 +0530

    aaa

--- scripts/get_vocab.py
+++ scripts/get_vocab.py
@@ -1,16 +1,17 @@
-import word2vec
+# import word2vec
+import fasttext
 import sys
 import os
 
 '''
 Usage: python get_vocab.py /path/to/vocab.bin
 '''
-w2v_file = sys.argv[1]
-model = word2vec.load(w2v_file)
-
-vocab =  model.vocab
+w2v_file = "/home/ubuntu/filestore/keshav/im2recipe"
+# model = word2vec.load(w2v_file)
+model = fasttext.load_model(w2v_file + "/im2recipe_myntra_vocab.bin")
+vocab =  model.words
 
 print("Writing to %s..." % os.path.join(os.path.dirname(w2v_file),'vocab.txt'))
-f = open(os.path.join(os.path.dirname(w2v_file),'vocab.txt'),'w')
+f = open(os.path.join(os.path.dirname(w2v_file),'im2recipe_myntra_vocab.txt'),'w')
 f.write("\n".join(vocab))
 f.close()
