90fbf8150b4a5833111d39a8e78e141f37c92132
Keshav <kshavgupta47@gmail.com>
2019-10-09 17:30:43 +0530

    aaa

--- scripts/get_vocab.py
+++ scripts/get_vocab.py
@@ -6,7 +6,7 @@ import os
 '''
 Usage: python get_vocab.py /path/to/vocab.bin
 '''
-w2v_file = "/home/ubuntu/filestore/keshav/im2recipe"
+w2v_file = "/home/ubuntu/filestore/keshav/im2recipe/data"
 # model = word2vec.load(w2v_file)
 model = fasttext.load_model(w2v_file + "/im2recipe_myntra_vocab.bin")
 vocab =  model.words
